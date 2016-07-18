8f29642575f97b0538e42307242078d40d862b84
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2016-07-19 02:34:04 +0800

    Copied code from promis example

--- 05.4 angular-services-data-sharing/public/index.js
+++ 05.4 angular-services-data-sharing/public/index.js
@@ -3,30 +3,31 @@
         .module("RegApp", [])
         .controller("RegCtrl", RegCtrl);
 
-    RegCtrl.$inject = ["$http"];
 
-    function RegCtrl($http) {
-        vm = this;
-        vm.employee = {};
-        vm.employee.firstname = "";
-        vm.employee.lastname = "";
-        vm.employee.gender = "";
-        vm.employee.birthday = "";
-        vm.employee.hiredate = "";
+    //Try removing DB service from this line? Does the console.log from service still gets printed??
+    RegCtrl.$inject = ["$http", "dbService"];
+
+    function RegCtrl($http, dbService) {
+        var vm = this;
+        vm.employee = dbService.getEmployee();
+
         vm.status = {
             message: "",
             code: 0
         };
 
         vm.register = function () {
-            $http.post("/api/employee/save", vm.employee).then(function (result) {
-                console.info(result);
-                vm.status.message = "Inserted successfully";
-                vm.status.code = 202;
-            }).catch(function () {
-                vm.status.message = "An error occured.";
-                vm.status.code = 400;
-            });
+            dbService.save(vm.employee)
+                .then(function (result) {
+                    vm.status.message = "Inserted successfully";
+                    vm.status.code = 202;
+                    console.log(result);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                    vm.status.message = "An error occurred.";
+                    vm.status.code = 400;
+                })
         }
     }
 })();
