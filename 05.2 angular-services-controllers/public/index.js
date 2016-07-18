900124f69b857667d3b4d52043af83c8af23bd9f
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2016-07-19 01:32:04 +0800

    Moved code from controller to service

--- 05.2 angular-services-controllers/public/index.js
+++ 05.2 angular-services-controllers/public/index.js
@@ -8,26 +8,26 @@
     RegCtrl.$inject = ["$http", "dbService"];
 
     function RegCtrl($http, dbService) {
-        vm = this;
-        vm.employee = {};
-        vm.employee.firstname = "";
-        vm.employee.lastname = "";
-        vm.employee.gender = "";
-        vm.employee.birthday = "";
-        vm.employee.hiredate = "";
+        var vm = this;
+        vm.employee = dbService.getEmployee();
+
         vm.status = {
             message: "",
             code: 0
         };
 
         vm.register = function () {
-            $http.post("/api/employee/save", vm.employee).then(function (result) {
-                console.info(result);
+            dbService(vm.employee).save(function (err, result) {
+                if (err) {
+                    vm.status.message = "An error occurred.";
+                    vm.status.code = 400;
+                    return console.log(err);
+                }
+
                 vm.status.message = "Inserted successfully";
                 vm.status.code = 202;
-            }).catch(function () {
-                vm.status.message = "An error occured.";
-                vm.status.code = 400;
+
+                console.log(result);
             });
         }
     }
--- 05.2 angular-services-controllers/public/service.js
+++ 05.2 angular-services-controllers/public/service.js
@@ -1,6 +1,28 @@
 (function () {
     angular.module("RegApp")
-        .service("dbService", function () {
-            console.log("Hey From DB service")
-        });
+        .service("dbService", dbService);
+
+    dbService.$inject = ["$http"];
+
+    function dbService($http) {
+        var ctrl = this;
+
+        ctrl.getEmployee = function () {
+            var employee = {};
+            employee.firstname = "";
+            employee.lastname = "";
+            employee.gender = "";
+            employee.birthday = "";
+            employee.hiredate = "";
+            return employee;
+        };
+
+        ctrl.save = function (employee, callback) {
+            $http.post("/api/employee/save", employee).then(function (result) {
+                callback(null, result);
+            }).catch(function () {
+                callback(null)
+            });
+        };
+    }
 })();
